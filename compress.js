1b5e010f6fb01b0bfa2cdae9fae8bd648bbe0a56
stephan <webmaster1@beyondjava.de>
2019-08-24 17:55:23 +0200

    added brotli compression

---
+++ compress.js
@@ -0,0 +1,42 @@
+const brotli = require('brotli');
+const fs = require('fs');
+
+var dir = require('node-dir');
+
+const start = new Date().getTime();
+
+dir.files('dist/pdf-showcase', function(err, files) {
+  if (err) throw err;
+  files.forEach(f => compress(f));
+  const end = new Date().getTime();
+  console.log('Compressing took ' + (end - start) / 1000 + ' seconds');
+});
+
+function compress(file) {
+  if (file.endsWith('.js') || file.endsWith('.css') || file.endsWith('.html') || file.endsWith('.pdf') || file.endsWith('.md')) {
+    const content = fs.readFileSync(file);
+    if (content.length > 1000) {
+      const result = brotli.compress(content, {
+        mode: 0, // 0 = generic, 1 = text, 2 = font (WOFF2)
+        quality: 11, // 0 - 11
+        lgwin: 22 // window size
+      });
+      if (result != null && result.length < content.length) {
+        fs.writeFileSync(file + '.br', result);
+        const reduction = content.length - result.length;
+        console.log(file + ' Reduction: ' + reduction + ' bytes ' + Math.round((reduction * 100) / content.length) + '%');
+      }
+    }
+  }
+}
+
+/*
+const content = fs.readFileSync('package.json');
+console.log(content);
+const result = brotli.compress(content, {
+  mode: 0, // 0 = generic, 1 = text, 2 = font (WOFF2)
+  quality: 11, // 0 - 11
+  lgwin: 22 // window size
+});
+fs.writeFileSync('README.md.br', result);
+*/
