5712608621c545679a291ebe23233f90bb74fce0
stephan <webmaster1@beyondjava.de>
2019-09-03 23:04:12 +0200

    #135 [base64Src] now redraws the PDF file when the input has changed; #126 up/down keypress shouldn't move the focus away from the current input field (didn't work in all cases);

--- src/app/base64/base64.component.ts
+++ src/app/base64/base64.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { NgxExtendedPdfViewerService } from 'ngx-extended-pdf-viewer';
 import { pdfBase64 } from './pdfBase64';
+import { pdfData2 } from './secondPdfBase64';
 
 @Component({
   selector: 'app-base64',
@@ -10,4 +11,12 @@ import { pdfBase64 } from './pdfBase64';
 })
 export class Base64Component {
   public base64 = pdfBase64;
+
+  public toggle(): void {
+    if (this.base64 === pdfBase64) {
+      this.base64 = pdfData2;
+    } else {
+      this.base64 = pdfBase64;
+    }
+  }
 }
