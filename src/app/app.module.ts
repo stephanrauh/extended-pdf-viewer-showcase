0ae59e89a252925d6f2c21296625e06cebc3bd7f
stephan <webmaster1@beyondjava.de>
2019-10-20 19:54:30 +0200

    updated to Angular 9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,21 +8,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { LayoutModule } from '@angular/cdk/layout';
-import {
-  MatToolbarModule,
-  MatButtonModule,
-  MatSidenavModule,
-  MatIconModule,
-  MatInputModule,
-  MatListModule,
-  MatCardModule,
-  MatFormFieldModule,
-  MatSelectModule,
-  MatTabsModule,
-  MatRadioModule,
-  MatCheckboxModule,
-  MatProgressBarModule
-} from '@angular/material';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { I18nComponent } from './i18n/i18n.component';
 import { PagesLoadedComponent } from './pages-loaded/pages-loaded.component';
@@ -45,7 +43,6 @@ import { ResponsiveDesignComponent } from './responsive-design/responsive-design
 import { SmartphoneComponent } from './smartphone/smartphone.component';
 import { ContextmenuComponent } from './contextmenu/contextmenu.component';
 import { CustomPrintDialogComponent } from './custom-print-dialog/custom-print-dialog.component';
-
 @NgModule({
   declarations: [
     AppComponent,
