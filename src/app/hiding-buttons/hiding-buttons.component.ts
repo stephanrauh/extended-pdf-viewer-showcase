827daeb80ce1aa612a4f123c4950c9dab4844493
stephan <webmaster1@beyondjava.de>
2020-03-22 19:15:27 +0100

    stephanrauh/ngx-extended-pdf-viewer#200 simplified and updated the demo

--- src/app/hiding-buttons/hiding-buttons.component.ts
+++ src/app/hiding-buttons/hiding-buttons.component.ts
@@ -22,4 +22,27 @@ export class HidingButtonsComponent {
   public showSpreadButton = false;
   public showPropertiesButton = false;
   public downloadFileName = 'user-defined-name.pdf';
+
+  public get sourcecode() {
+    return `<ngx-extended-pdf-viewer
+  [src]="'assets/pdfs/pdf-sample.pdf'"
+  [showSidebarButton]="${this.showSidebarButton}"
+  [showFindButton]="${this.showFindButton}"
+  [showPagingButtons]="${this.showPagingButtons}"
+  [showZoomButtons]="${this.showZoomButtons}"
+  [showPresentationModeButton]="${this.showPresentationModeButton}"
+  [showOpenFileButton]="${this.showOpenFileButton}"
+  [showPrintButton]="${this.showPrintButton}"
+  [showDownloadButton]="${this.showDownloadButton}"
+  [showBookmarkButton]="${this.showBookmarkButton}"
+  [showSecondaryToolbarButton]="${this.showSecondaryToolbarButton}"
+  [showRotateButton]="${this.showRotateButton}"
+  [showHandToolButton]="${this.showHandToolButton}"
+  [showScrollingButton]="${this.showScrollingButton}"
+  [showSpreadButton]="${this.showSpreadButton}"
+  [showPropertiesButton]="${this.showPropertiesButton}"
+  [useBrowserLocale]="true" height="250px" zoom="25%">
+</ngx-extended-pdf-viewer>
+`;
+  }
 }
