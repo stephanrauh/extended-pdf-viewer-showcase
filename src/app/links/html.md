d23f43903749f71a64d20806a8e12549663911ee
stephan <webmaster1@beyondjava.de>
2020-05-19 17:24:39 +0200

    stephanrauh/ngx-extended-pdf-viewer#262 fine tuning the demo

---
+++ src/app/links/html.md
@@ -0,0 +1,8 @@
+```html
+<ngx-extended-pdf-viewer *ngIf="!hidden"
+  [src]="'assets/pdfs/blind-text-collection.pdf'"
+  backgroundColor="#ffffff"
+  [height]="'90vh'"
+  [useBrowserLocale]="true">
+</ngx-extended-pdf-viewer>
+```
--- src/app/links/links.component.ts
+++ src/app/links/links.component.ts
@@ -1,25 +1,62 @@
 import { Component, OnInit } from '@angular/core';
-import { defaultOptions } from 'ngx-extended-pdf-viewer';
-import { LinkTarget } from 'ngx-extended-pdf-viewer';
+import { defaultOptions, LinkTarget } from 'ngx-extended-pdf-viewer';
 @Component({
   selector: 'app-links',
   templateUrl: './links.component.html',
-  styleUrls: ['./links.component.css']
+  styleUrls: ['./links.component.css'],
 })
 export class LinksComponent implements OnInit {
+  public LinkTarget = LinkTarget;
+
+  public hidden = false;
+
+  private _target;;
 
   constructor() {}
 
-  ngOnInit() {
-    defaultOptions.externalLinkTarget = LinkTarget.BLANK;
+  public set target(t: number) {
+    if (this._target !== t) {
+      this._target = t;
+      this.hidden = true;
+      defaultOptions.externalLinkTarget = t;
+      setTimeout(() => (this.hidden = false), 250);
+    }
+  }
+
+  public get target(): number {
+    return this._target;
+  }
+
+  public ngOnInit() {
+    this.target = LinkTarget.BLANK;
+
   }
 
   public get sourcecode(): string {
+    let target: string;
+    switch (this._target) {
+      case LinkTarget.BLANK:
+        target = 'BLANK';
+        break;
+      case LinkTarget.NONE:
+        target = 'NONE';
+        break;
+      case LinkTarget.PARENT:
+        target = 'PARENT';
+        break;
+      case LinkTarget.SELF:
+        target = 'SELF';
+        break;
+      case LinkTarget.TOP:
+        target = 'TOP';
+        break;
+    }
+
     return `import { defaultOptions } from 'ngx-extended-pdf-viewer/default-options';
-    import { LinkTarget } from 'ngx-extended-pdf-viewer';
-    ...
-    ngOnInit(): void {
-      defaultOptions.externalLinkTarget = LinkTarget.BLANK;
-    }`;
+import { LinkTarget } from 'ngx-extended-pdf-viewer';
+...
+ngOnInit(): void {
+  defaultOptions.externalLinkTarget = LinkTarget.${target};
+}`;
   }
 }
