de91f2dc52bb747cf2874513bc504df01d2a7ebe
stephan <webmaster1@beyondjava.de>
2020-07-23 23:28:29 +0200

    stephanrauh/ngx-extended-pdf-viewer#400 assign different colors to the thumbnails of the front pages, the table of contents, and the rest of the text

--- src/app/custom-thumbnails/custom-thumbnails.component.ts
+++ src/app/custom-thumbnails/custom-thumbnails.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PdfThumbnailDrawnEvent } from '../../../../ngx-extended-pdf-viewer/projects/ngx-extended-pdf-viewer/src/lib/events/pdf-thumbnail-drawn-event';
 
 (window as any).updateThumbnailSelection = (page: number) => {
   setTimeout(() => {
@@ -31,4 +32,18 @@ export class CustomThumbnailsComponent implements OnInit {
       }
     }
   }
+
+  public onThumbnailDrawn(thumbnailEvent: PdfThumbnailDrawnEvent): void {
+    console.log('Thumnail drawn ' + thumbnailEvent);
+    const thumbnail = thumbnailEvent.thumbnail;
+    const page = thumbnailEvent.pageId;
+    const overlay = thumbnail.querySelector('.image-container') as HTMLElement;
+    if (page <= 2) {
+      overlay.style.backgroundColor = '#0000FF40';
+    } else if (page === 3 || page === 4) {
+      overlay.style.backgroundColor = '#00FF0040';
+    } else {
+      overlay.style.backgroundColor = '#FF000040';
+    }
+   }
 }
