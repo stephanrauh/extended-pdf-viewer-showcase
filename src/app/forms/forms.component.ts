c505398f4a383a6ee97ff0ba875ff5af4948f080
stephan <webmaster1@beyondjava.de>
2020-10-21 23:26:21 +0200

    stephanrauh/ngx-extended-pdf-viewer#499 and stephanrauh/ngx-extended-pdf-viewer#511 two-way binding of form data (PoC)

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -11,17 +11,26 @@ export class FormsComponent {
   public firstName = 'Jane';
 
   public lastName = 'Doe';
+  public country = 'Germany';
+  public jobExperience = '6';
+  public typeScript = true;
 
-  public get formData(): { [fieldName: string]: string } {
+  public get formData(): { [fieldName: string]: string | number | boolean } {
     return {
       firstName: this.firstName,
       lastName: this.lastName,
+      yearsOfExperience: this.jobExperience,
+      typeScript: this.typeScript,
+      country: this.country
     };
   }
 
-  public set formData(data: { [fieldName: string]: string }) {
-    this.firstName = data.firstName;
-    this.lastName = data.lastName;
+  public set formData(data: { [fieldName: string]: string | number | boolean }) {
+    this.firstName = data.firstName as string;
+    this.lastName = data.lastName as string;
+    this.jobExperience = data.yearsOfExperience as string;
+    this.country = data.country as string;
+    this.typeScript = data.typeScript === 'true';
   }
 
   constructor() {}
