85d8e68ab6ba17cdd6b84c6b0c3c9bec09adec96
unknown <jmpspm@yahoo.com>
2020-12-01 00:12:37 -0500

    Update the Showcase with examples of the new PDF Form Field Types

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -9,33 +9,42 @@ import { Component } from '@angular/core';
 export class FormsComponent {
   public selectedTab = 0;
 
-  public firstName = 'Lucía';
+  public firstName = 'LucÃ­a';
 
   public lastName = 'Garzas';
   public country = 'Spain';
   public jobExperience = '6';
-  public typeScript = true;
+  public typeScript = 'Yes';
+  public databases = ['oracle','db2'];
+  public educationLevel = 'bachelorDegree';
+  public otherJobExperience = 'Several\nOther\nJobs';
 
   public downloaded: string | undefined;
 
   public rawFormData: any[];
 
-  public get formData(): { [fieldName: string]: string | number | boolean } {
+  public get formData(): { [fieldName: string]: string | string[] | number | boolean } {
     return {
       firstName: this.firstName,
       lastName: this.lastName,
       yearsOfExperience: this.jobExperience,
       typeScript: this.typeScript,
-      country: this.country
+      country: this.country,
+      databases: this.databases,
+      educationLevel: this.educationLevel,
+      otherJobExperience: this.otherJobExperience,
     };
   }
-
-  public set formData(data: { [fieldName: string]: string | number | boolean }) {
+ 
+  public set formData(data: { [fieldName: string]: string | string[] | number | boolean }) {
     this.firstName = data.firstName as string;
     this.lastName = data.lastName as string;
     this.jobExperience = data.yearsOfExperience as string;
     this.country = data.country as string;
-    this.typeScript = data.typeScript === 'true' || data.typeScript === true;
+    this.databases = data.databases as string[],
+    this.educationLevel = data.educationLevel as string,
+    this.otherJobExperience = data.otherJobExperience as string
+    this.typeScript = data.typeScript as string;
   }
 
   constructor(private ngxService: NgxExtendedPdfViewerService) {}
