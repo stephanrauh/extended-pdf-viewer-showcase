811cd018305db798b2c3311ad108f6d3cc136b9c
stephan <webmaster1@beyondjava.de>
2021-06-27 22:58:33 +0200

    updated to version 9.0.3

--- src/app/nav/versions.ts
+++ src/app/nav/versions.ts
@@ -1,6 +1,6 @@
 export const versions = {
-  angular: '12.0.3',
-  extendedPdfViewer: '9.0.1',
+  angular: '12.1.0',
+  extendedPdfViewer: '9.0.3',
   ng2PdfViewer: '7.0.1',
   pdfJsDist: '2.7.570'
 } 
