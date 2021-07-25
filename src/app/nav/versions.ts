d31adb02bc70cf380908176d80da159530306ed0
stephan <webmaster1@beyondjava.de>
2021-07-25 18:50:19 +0200

    stephanrauh/ngx-extended-pdf-viewer#847 update to pdf.js 2.10/2.11

--- src/app/nav/versions.ts
+++ src/app/nav/versions.ts
@@ -1,6 +1,6 @@
 export const versions = {
-  angular: '12.1.1',
-  extendedPdfViewer: '10.0.0-alpha.1',
+  angular: '12.1.3',
+  extendedPdfViewer: '10.0.0-alpha.2',
   ng2PdfViewer: '7.0.1',
   pdfJsDist: '2.7.570'
 } 
