b2f45d43d9b886bacee329fe5c4435b8b80ca16c
stephan <webmaster1@beyondjava.de>
2021-08-11 00:03:21 +0200

    document how to disable the script engine

--- src/app/nav/versions.ts
+++ src/app/nav/versions.ts
@@ -1,6 +1,6 @@
 export const versions = {
-  angular: '12.1.3',
-  extendedPdfViewer: '10.0.0-alpha.2',
+  angular: '12.2.0',
+  extendedPdfViewer: '10.0.0-alpha.5',
   ng2PdfViewer: '7.0.1',
   pdfJsDist: '2.7.570'
 } 
