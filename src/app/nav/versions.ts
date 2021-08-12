cf57252495a1bf821e140ec6e205dea724791cd1
stephan <webmaster1@beyondjava.de>
2021-08-12 23:05:10 +0200

    stephanrauh/ngx-extended-pdf-viewer#859 combine pageViewMode: single and spread: even

--- src/app/nav/versions.ts
+++ src/app/nav/versions.ts
@@ -1,6 +1,6 @@
 export const versions = {
-  angular: '12.2.0',
-  extendedPdfViewer: '10.0.0-alpha.5',
+  angular: '12.2.1',
+  extendedPdfViewer: '10.0.0-alpha.6',
   ng2PdfViewer: '7.0.1',
   pdfJsDist: '2.7.570'
 } 
