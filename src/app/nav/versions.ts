6f17009ce370771a84f5ec89a25b94f652fabe21
stephan <webmaster1@beyondjava.de>
2021-09-13 22:58:32 +0200

    dependency updates

--- src/app/nav/versions.ts
+++ src/app/nav/versions.ts
@@ -1,6 +1,6 @@
 export const versions = {
-  angular: '12.2.3',
-  extendedPdfViewer: '10.0.0-alpha.9',
+  angular: '12.2.5',
+  extendedPdfViewer: '10.0.0-alpha.11',
   ng2PdfViewer: '7.0.1',
   pdfJsDist: '2.7.570'
 } 
