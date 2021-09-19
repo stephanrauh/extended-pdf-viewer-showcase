337bff5b368cb3158ea05edfadd748a3664c9120
stephan <webmaster1@beyondjava.de>
2021-09-19 16:41:13 +0200

    published version 10.0.0

--- src/app/nav/extended-pdf-viewer-menu/extended-pdf-viewer-menu.component.ts
+++ src/app/nav/extended-pdf-viewer-menu/extended-pdf-viewer-menu.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { filter, tap } from 'rxjs';
 
 @Component({
   selector: 'app-extended-pdf-viewer-menu',
   templateUrl: './extended-pdf-viewer-menu.component.html',
-  styleUrls: ['./extended-pdf-viewer-menu.component.css']
+  styleUrls: ['./extended-pdf-viewer-menu.component.css'],
 })
 export class ExtendedPdfViewerMenuComponent implements OnInit {
-
-  constructor() { }
+  constructor(private router: Router, private cd: ChangeDetectorRef) {}
 
   ngOnInit(): void {
+    setTimeout(() => {
+      const currentUrl = this.router.routerState.snapshot.url;
+      this.highlightActiveMenu(currentUrl);
+    },500);
+
+    this.router.events
+      .pipe(
+        filter((event) => event.constructor.name === 'NavigationEnd'),
+        tap((event: any) => {
+          const url = event.url;
+          this.highlightActiveMenu(url);
+        })
+      )
+      .subscribe();
   }
 
+  private highlightActiveMenu(url: any) {
+    const menuItems = document.querySelectorAll('a.mat-list-item');
+    menuItems.forEach((item) => {
+      let href = (item as HTMLAnchorElement).href;
+      const pos = href.lastIndexOf('/');
+      if (pos >= 0) {
+        href = href.substring(pos + 1);
+      }
+      const active = url.endsWith(href);
+      if (active) {
+        console.log(href);
+        item.classList.add('active');
+      } else {
+        item.classList.remove('active');
+      }
+    });
+  }
 }
