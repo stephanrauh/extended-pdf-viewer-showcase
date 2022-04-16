f98774dcfbe1413e2a17cfff15deb41a98371b9e
Stephan Rauh <3045767+stephanrauh@users.noreply.github.com>
2022-04-16 14:35:40 +0200

    dependency updates

--- src/app/nav/versions.ts
+++ src/app/nav/versions.ts
@@ -1,6 +1,6 @@
 export const versions = {
-  angular: '13.3.2',
-  extendedPdfViewer: '13.0.0-alpha.6',
-  ng2PdfViewer: '8.0.1',
-  pdfJsDist: '2.11.338'
+  angular: '13.3.3',
+  extendedPdfViewer: '13.0.0-alpha.11',
+  ng2PdfViewer: '9.0.0',
+  pdfJsDist: '2.13.216'
 } 
