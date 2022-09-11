2c72092fc51dc6dd473ba623d8e9f1e0b9882375
Stephan Rauh <3045767+stephanrauh@users.noreply.github.com>
2022-09-11 22:45:15 +0200

    published version 15.0.0

--- src/app/nav/versions.ts
+++ src/app/nav/versions.ts
@@ -1,6 +1,6 @@
 export const versions = {
   angular: '14.2.1',
-  extendedPdfViewer: '15.0.0-beta.0',
-  ng2PdfViewer: '9.1.1',
+  extendedPdfViewer: '15.0.0',
+  ng2PdfViewer: '9.1.2',
   pdfJsDist: '2.14.305'
 } 
