5cb62381be81f7e1b65ce8258b4a35cd6732de93
Stephan Rauh <3045767+stephanrauh@users.noreply.github.com>
2023-04-16 16:30:37 +0200

    #1735 rewrite form support from scratch

--- src/app/extended-pdf-viewer/forms/forms.component.ts
+++ src/app/extended-pdf-viewer/forms/forms.component.ts
@@ -3,6 +3,7 @@ import { countries } from './countries';
 import { ChangeDetectionStrategy, Component, OnChanges, SimpleChanges } from '@angular/core';
 import { MatTabChangeEvent } from '@angular/material/tabs';
 import { isLocalhost } from '../common/utilities';
+import { MatCheckboxChange } from '@angular/material/checkbox';
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
@@ -12,9 +13,13 @@ import { isLocalhost } from '../common/utilities';
 export class FormsComponent {
   public selectedTab = 0;
   public theme = 'light';
+  public xfa = false;
 
   public firstName = 'Lucía';
 
+  public visible = true;
+
+  // Acroform fields:
   public lastName = 'Garzas';
   public country = 'Spain';
   public jobExperience = '6';
@@ -27,13 +32,33 @@ export class FormsComponent {
   public otherJobExperience = 'Several\nOther\nJobs';
   public countries = countries;
 
+  // XFA fields:
+  public serviceIn = '01';
+  public familyName = 'Du Bois';
+  public givenName = 'Michelle';
+  public aliasNameIndicator = 'Y';
+  public canadaUS = '1';
+  public other = '1';
+  public dateOfBirthYear = 1970;
+
   public downloaded: string | undefined;
 
   public rawFormData!: any[];
 
   private _fullscreen = false;
 
+  public src = '/assets/pdfs/OoPdfFormExample.pdf';
+
+  public formData: {
+    [fieldName: string]: string | string[] | number | boolean;
+  } = {};
+
+  public xfaFormData: {
+    [fieldName: string]: string | string[] | number | boolean;
+  } = {};
+
   public isLocalhost = isLocalhost();
+  public leftTab = 0;
 
   public get fullscreen(): boolean {
     return this._fullscreen;
@@ -44,28 +69,57 @@ export class FormsComponent {
     setTimeout(() => this.ngxService.recalculateSize());
   }
 
-  public formData: {
-    [fieldName: string]: string | string[] | number | boolean;
-  } = {};
-
   public updateFormData(): void {
-    this.formData = {
-      ...this.formData,
-      firstName: this.firstName,
-      lastName: this.lastName,
-      yearsOfExperience: this.jobExperience,
-      typeScript: this.typeScript,
-      javaScript: this.javaScript,
-      java: this.java,
-      cSharp: this.cSharp,
-      country: this.country,
-      databases: this.databases,
-      educationLevel: this.educationLevel,
-      otherJobExperience: this.otherJobExperience,
-    };
+    if (this.xfa) {
+      this.xfaFormData = {
+        ...this.xfaFormData,
+        // XFA form fields:
+        ServiceIn: this.serviceIn,
+        FamilyName: this.familyName,
+        GivenName: this.givenName,
+        AliasNameIndicator: this.aliasNameIndicator,
+        CanadaUS: this.canadaUS,
+        Other: this.other,
+        DOBYear: this.dateOfBirthYear,
+      };
+    } else {
+      this.formData = {
+        ...this.formData,
+
+        // Acroform fields:
+        firstName: this.firstName,
+        lastName: this.lastName,
+        yearsOfExperience: this.jobExperience,
+        typeScript: this.typeScript,
+        javaScript: this.javaScript,
+        java: this.java,
+        cSharp: this.cSharp,
+        country: this.country,
+        databases: this.databases,
+        educationLevel: this.educationLevel,
+        otherJobExperience: this.otherJobExperience,
+
+        // XFA form fields:
+        FamilyName: this.familyName,
+        GivenName: this.givenName,
+        AliasNameIndicator: this.aliasNameIndicator,
+        CanadaUS: this.canadaUS,
+        Other: this.other,
+      };
+    }
   }
 
   public setFormData(data: { [fieldName: string]: string | string[] | number | boolean } | any) {
+    if (this.xfa) {
+      this.formData = data;
+      this.serviceIn = data['ServiceIn'];
+      this.familyName = data['FamilyName'];
+      this.givenName = data['GivenName'];
+      this.aliasNameIndicator = data['AliasNameIndicator'];
+      this.canadaUS = data['CanadaUS'];
+      this.other = data['Other'];
+      this.dateOfBirthYear = data['DOBYear'];
+    } else {
       this.firstName = data.firstName as string;
       this.lastName = data.lastName as string;
       this.jobExperience = data.yearsOfExperience as string;
@@ -78,9 +132,16 @@ export class FormsComponent {
       this.java = Boolean(data.java);
       this.cSharp = Boolean(data.cSharp);
       this.updateFormData();
+    }
   }
 
   constructor(private ngxService: NgxExtendedPdfViewerService) {
+    this.onSelectTab({ index: 4 } as MatTabChangeEvent);
+    this.updateFormData();
+  }
+
+  public setCanadaUs(event: MatCheckboxChange): void {
+    this.canadaUS = event.checked ? '1' : '0';
     this.updateFormData();
   }
 
@@ -121,12 +182,22 @@ export class FormsComponent {
   }
 
   public onSelectTab(event: MatTabChangeEvent): void {
+    this.leftTab = event.index;
     if (event.index === 1) {
       this.selectedTab = 2;
     } else if (event.index === 2) {
       this.selectedTab = 4;
     } else if (event.index === 3) {
       this.selectedTab = 5;
+    } else if (event.index === 4) {
+      this.formData = {};
+      this.visible = false;
+      setTimeout(() => {
+        this.src = '/assets/pdfs/under-copyright/XFA-Canada-Immigration.pdf';
+        this.visible = true;
+      });
+      this.xfa = true;
+      this.updateFormData();
     }
   }
 }
