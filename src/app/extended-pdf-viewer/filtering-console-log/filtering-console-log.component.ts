65cfd6c9ee24a7d0a23199ed240917b77abb155b
Stephan Rauh <3045767+stephanrauh@users.noreply.github.com>
2023-06-03 20:27:31 +0200

    stephanrauh/ngx-extended-pdf-viewer#1672 re-activated filtering the console output (was broken after implementing support for server-side rendering)

--- src/app/extended-pdf-viewer/filtering-console-log/filtering-console-log.component.ts
+++ src/app/extended-pdf-viewer/filtering-console-log/filtering-console-log.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { NgxExtendedPdfViewerService } from 'ngx-extended-pdf-viewer';
 import { isLocalhost } from '../common/utilities';
+import { WindowRefService } from 'src/app/window-ref.servce';
 
 @Component({
   selector: 'app-filtering-console-log',
@@ -24,13 +25,15 @@ export class FilteringConsoleLogComponent {
     this.pdfService.recalculateSize());
   }
 
-  constructor(private pdfService: NgxExtendedPdfViewerService) {
-    Window['ngxConsoleFilter'] = (level: string, message: any): boolean => {
-      if (message?.includes && message?.includes('running on')) {
-        this.version = message;
-        return false;
-      }
-      return true;
+  constructor(private pdfService: NgxExtendedPdfViewerService, @Inject(PLATFORM_ID) private platformId: any, private windowRefService: WindowRefService) {
+    if (windowRefService.nativeWindow) {
+      globalThis['ngxConsoleFilter'] = (level: string, message: any): boolean => {
+        if (message?.includes && message?.includes('modified by ngx-extended-pdf-viewer')) {
+          this.version = message;
+          return false;
+        }
+        return true;
+      };
     }
   }
 }
