0a0d1ced0e366f3fd2ddcf29f03b35408df6d525
Stephan Rauh <3045767+stephanrauh@users.noreply.github.com>
2023-06-08 17:16:41 +0200

    stephanrauh/ngx-extended-pdf-viewer#1707 added a demo explaining the `left` parameter

--- src/app/extended-pdf-viewer/scrolling/scrolling.component.ts
+++ src/app/extended-pdf-viewer/scrolling/scrolling.component.ts
@@ -13,6 +13,10 @@ export class ScrollingComponent {
 
   private _fullscreen = false;
 
+  public selectedTab = 1;
+
+  public zoom="page-width";
+
   public get fullscreen(): boolean {
     return this._fullscreen;
   }
@@ -22,9 +26,25 @@ export class ScrollingComponent {
     setTimeout(() => this.pdfService.recalculateSize());
   }
 
-  constructor(private pdfService: NgxExtendedPdfViewerService) { }
+  public get src(): string {
+    if (this.selectedTab === 0) {
+      return './assets/pdfs/The Public Domain - Enclosing the Commons of the Mind.pdf';
+    } else {
+      return './assets/pdfs/issue1707-with-rulers.pdf';
+    }
+  }
+
+  constructor(private pdfService: NgxExtendedPdfViewerService) {}
 
   public scroll(pageNumber: number, top: number | string): void {
-    this.pdfService.scrollPageIntoView(pageNumber, {top});
+    this.pdfService.scrollPageIntoView(pageNumber, { top });
+  }
+
+  public scrollLeft(pageNumber: number, left: string): void {
+    this.pdfService.scrollPageIntoView(pageNumber, { left });
+  }
+
+  public scrollRight(pageNumber: number, right: string): void {
+    // this.pdfService.scrollPageIntoView(pageNumber, { right });
   }
 }
--- src/app/nav/versions.ts
+++ src/app/nav/versions.ts
@@ -1,4 +1,4 @@
 export const versions = {
   angular: '15.2.8',
-  extendedPdfViewer: '17.0.0-alpha.10',
+  extendedPdfViewer: '17.0.0-alpha.11',
 } 
