4d77e045938244b432b19f7d8bdf482d43de40da
Stephan Rauh <3045767+stephanrauh@users.noreply.github.com>
2023-07-08 16:11:13 +0200

    update to version 18.0.0

--- src/app/extended-pdf-viewer/find/find.component.ts
+++ src/app/extended-pdf-viewer/find/find.component.ts
@@ -11,17 +11,11 @@ import { isLocalhost } from '../common/utilities';
 export class FindComponent {
   // tslint:disable-next-line:variable-name
   public _searchtext = '';
-  public _searchtext2 = '';
-  public _searchtext3 = '';
 
-  public fuzzy = false;
   public highlightAll = false;
-  public currentPage = false;
-  private _pageRange = '';
   public matchCase = false;
   public wholeWord = false;
   public ignoreAccents = false;
-  public multipleSearchTerms = false;
 
   public currentMatchNumber: number | undefined;
 
@@ -53,15 +47,6 @@ export class FindComponent {
     setTimeout(() => this.ngxExtendedPdfViewerService.recalculateSize());
   }
 
-  public get pageRange(): string {
-    return this._pageRange;
-  }
-
-  public set pageRange(pageRange: string) {
-    this._pageRange = pageRange;
-    this.find();
-  }
-
   public get findStateText(): string {
     switch (this.findState) {
       case FindState.FOUND:
@@ -91,46 +76,12 @@ export class FindComponent {
       this.currentMatchNumber = undefined;
       this.totalMatches = undefined;
     }
-    if (this.multipleSearchTerms) {
-      this.ngxExtendedPdfViewerService.findMultiple([this._searchtext, this.searchtext2, this._searchtext3], {
-        highlightAll: this.highlightAll,
-        currentPage: this.currentPage,
-        matchCase: this.matchCase,
-        wholeWords: this.wholeWord,
-        ignoreAccents: this.ignoreAccents,
-        fuzzySearch: this.fuzzy,
-        pageRange: this.pageRange,
-        findMultipleSearchTexts: this.multipleSearchTerms
-      } as FindOptions);
-    } else {
-        this.ngxExtendedPdfViewerService.find(this._searchtext, {
-          highlightAll: this.highlightAll,
-          currentPage: this.currentPage,
-          matchCase: this.matchCase,
-          wholeWords: this.wholeWord,
-          ignoreAccents: this.ignoreAccents,
-          fuzzySearch: this.fuzzy,
-          pageRange: this.pageRange,
-          findMultipleSearchTexts: this.multipleSearchTerms
-        })
-    }
-  }
-
-  public get searchtext2(): string {
-    return this._searchtext2;
-  }
-
-  public set searchtext2(text: string) {
-    this._searchtext2 = text;
-    this.find();
-  }
-  public get searchtext3(): string {
-    return this._searchtext3;
-  }
-
-  public set searchtext3(text: string) {
-    this._searchtext3 = text;
-    this.find();
+    this.ngxExtendedPdfViewerService.find(this._searchtext, {
+      highlightAll: this.highlightAll,
+      matchCase: this.matchCase,
+      wholeWords: this.wholeWord,
+      ignoreAccents: this.ignoreAccents,
+    });
   }
 
   public isLocalhost = isLocalhost();
@@ -153,13 +104,9 @@ export class FindComponent {
   public onCheckboxClicked() {
     this.ngxExtendedPdfViewerService.find(this._searchtext, {
       highlightAll: this.highlightAll,
-      currentPage: this.currentPage,
       matchCase: this.matchCase,
       wholeWords: this.wholeWord,
       ignoreAccents: this.ignoreAccents,
-      fuzzySearch: this.fuzzy,
-      pageRange: this.pageRange,
-      findMultipleSearchTexts: this.multipleSearchTerms
     });
   }
 
@@ -173,16 +120,15 @@ export class FindComponent {
 
   public resetFindResult(): void {
     const allSpans = document.querySelectorAll('.textLayer > span') as NodeList;
-    allSpans.forEach((span, index) => {
+    allSpans.forEach((span) => {
       (span as HTMLElement).classList.remove('fade-out');
     });
   }
 
-  public onUpdateFindResult(event: FindResultMatchesCount): void {
+  private onUpdateFindResult(event: FindResultMatchesCount): void {
     const PDFViewerApplication: IPDFViewerApplication = (window as any).PDFViewerApplication;
     const matchIndexes = event.matches as Array<Array<number>>;
     const matchesLengths = event.matchesLength as Array<Array<number>>;
-    const matchesColors = event.matchesColor as Array<Array<number>>;
 
     setTimeout(() => {
       matchIndexes.forEach((findings, page) => {
@@ -190,7 +136,7 @@ export class FindComponent {
           const currentPage = PDFViewerApplication.pdfViewer._pages[page];
           if (currentPage.textHighlighter.textDivs) {
             if (page && matchesLengths[page][0] > 0) {
-              const converted = currentPage.textHighlighter._convertMatches([matchIndexes[page]], [matchesLengths[page]], [matchesColors[page]]) as Array<any>;
+              const converted = currentPage.textHighlighter._convertMatches([matchIndexes[page]], [matchesLengths[page]]) as Array<any>;
               const allSpans = currentPage.div.querySelectorAll('.textLayer > span') as NodeList;
               allSpans.forEach((span, index) => {
                 if (converted.some((highlight) => index >= highlight.begin.divIdx && index <= highlight.end.divIdx)) {
--- src/app/nav/versions.ts
+++ src/app/nav/versions.ts
@@ -1,4 +1,4 @@
 export const versions = {
   angular: '15.2.9',
-  extendedPdfViewer: '17.4.3',
+  extendedPdfViewer: '18.0.0-alpha.0',
 } 
