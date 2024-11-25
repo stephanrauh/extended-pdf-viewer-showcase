c44da0af87703f6995bde1ff5ddb0a7b8892950a
Stephan Rauh <3045767+stephanrauh@users.noreply.github.com>
2024-11-25 23:11:58 +0100

    update to Angular 19 - temporarily removed server-side rendering

--- src/app/app.common.module.ts
+++ src/app/app.common.module.ts
@@ -4,20 +4,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { SharedModule } from './shared/shared.module';
 
 
 @NgModule({
   imports: [
     BrowserAnimationsModule,
-    provideClientHydration({ appId: 'serverApp' }),
     SharedModule,
     AppRoutingModule,
-  ],
-  providers: [
-    { provide: 'APP_ID', useValue: 'serverApp' },
-    // other providers
   ],
   exports: [
     BrowserAnimationsModule,
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 
 @Component({
 standalone: false,
-standalone: false,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
