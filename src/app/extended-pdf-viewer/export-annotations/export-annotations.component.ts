7822a1f5c64d39b754fe0f935a66cd8b855525d3
Stephan Rauh <3045767+stephanrauh@users.noreply.github.com>
2025-03-08 21:16:50 +0100

    update to pdf.js 4.10

--- src/app/extended-pdf-viewer/export-annotations/export-annotations.component.ts
+++ src/app/extended-pdf-viewer/export-annotations/export-annotations.component.ts
@@ -60,26 +60,6 @@ export class ExportAnnotationsComponent {
     }
   }
 
-  public async addDrawing(): Promise<void> {
-    const x = 400 * Math.random();
-    const y = 350 + 500 * Math.random();
-    const drawing: InkEditorAnnotation = {
-      annotationType: 15,
-      color: [Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255)],
-      thickness: Math.random() * 10,
-      opacity: 1,
-      paths: [
-        {
-          bezier: [x + 0.5, y, x + 0.5, y + 44, x + 44, y + 66, x + 88, y + 44],
-          points: [x + 0.5, y, x + 0.5, y + 44],
-        },
-      ],
-      pageIndex: 0,
-      rect: [x, y, x + 100, y + 66],
-      rotation: 0,
-    };
-    await this.pdfViewerService.addEditorAnnotation(drawing);
-  }
 
   public removeEditors(): void {
     this.pdfViewerService.removeEditorAnnotations();
--- src/app/nav/versions.ts
+++ src/app/nav/versions.ts
@@ -1,4 +1,4 @@
 export const versions = {
   angular: '19.2.0',
-  extendedPdfViewer: '23.0.0-alpha.3',
+  extendedPdfViewer: '23.0.0-alpha.4',
 } 
